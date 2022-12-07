1a7cc2be72b7f703a64856c7e0180790b0e6a91f
sai1781 <96867450+sai1781@users.noreply.github.com>
2022-12-07 08:20:51 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -151,7 +151,7 @@ window.addEventListener('resize', () => {
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 // camera.position.x = 0.3
-// camera.position.y = 1.5
+camera.position.y = 1.5
 camera.position.z = 5
 scene.add(camera)
 
